95aed983f01b62a7feb51647b94c41b504f22d65
Victor Sergeyev <victor17011998@yandex.ru>
2018-02-05 22:00:36 +0400

    admin dir

---
+++ admin/index.php
@@ -0,0 +1,11 @@
+<?php
+
+// DEBUG
+ini_set('error_reporting', E_ALL | E_NOTICE | E_STRICT | E_DEPRECATED);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+ini_set('html_errors', 1);
+ini_set('docref_root', 'http://php.net/manual/ru/');
+ini_set('docref_ext', '.php');
+
+echo 'Вы вошли';
